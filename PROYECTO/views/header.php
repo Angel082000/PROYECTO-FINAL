7ef8af2a73e4af6d5c350a0c89791307e9612580
ANGEL COYOC <82916204+Angel082000@users.noreply.github.com>
2021-11-16 22:49:32 -0600

    Add files via upload

---
+++ PROYECTO/views/header.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!-- Navegación Header -->
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper blue darken-4">
+
+            <a href="#" class="brand-logo right"><img src="images/campeche.png" alt="" width="290" height="70"  /></a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="?menu=home">Inicio</a></li>
+                <li><a href="?menu=acerca">Acerca de</a></li>
+                <li><a href="?menu=productos">Productos</a></li>
+                <li><a href="?menu=registrate">Contacto</a></li>
+                <li><a href="sesion/index.php">Inicia Sesion</a></li>
+
+
+
+            </ul>
+        </div>
+    </nav>
+</div>
